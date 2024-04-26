f6516c5a9b5056db3e6d822f8528925088405e56
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-04-26 06:23:57 +0300

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe () {
+
+    function handlefocus () {
+        console.log("Good!");
+    }
+
+    function handleBlur () {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button onFocus={handlefocus} onBlur={handleBlur} >Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleChange(){
+        console.log("Entering password...");
+    }
+
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password"
+            name="password"
+            onChange={handleChange}
+            placeholder="Enter Password" 
+            />
+        </div>
     )
 }
 
